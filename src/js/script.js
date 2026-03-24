01cdcdd5d5317a0fd6d5d78519bc7c63ddd2da33
Vinycius <vinyciuslu@gmail.com>
2026-03-24 11:44:34 -0300

    feat: declarações de variaveis

---
+++ src/js/script.js
@@ -0,0 +1,47 @@
+// Declarações e Variaveis 
+
+//var, let e const
+
+// var x= 10;
+// console.log(x)
+// var x= 20;
+// console.log(x)
+//var - pode redeclarar e acessar fora do escopo 
+//let não pode redeclarar e não consegue acessar fora do escopo 
+// if(true) {
+//     var nome="Fiap";
+// }
+// console.log(nome)
+
+//const
+// const taxaJuros =0.85;
+// taxaJuros =0.50;
+// console.log(taxaJuros);
+
+var exemplo1="Ola Dev";
+console.log(exemplo1);
+
+let exemplo2="Ola dev let";
+console.log(exemplo2);
+
+const exemplo3="Ola Dev const";
+console.log(exemplo3);
+
+let exemplo4= 29;
+console.log(exemplo4);
+
+let exemplo5=true;
+console.log(exemplo5);
+
+let exemplo6={nome:"fiap"};
+console.log(exemplo6);
+
+let exemplo7=["Java", "Python", "C#"];
+console.log(exemplo7);
+
+// Variavel indefinida 
+let exemplo8; 
+console.log(exemplo8);
+
+let exemplo9=null;
+console.log(exemplo9);
